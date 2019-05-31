5628dc408de0a6315e8e0871a8a2bd669fd3c5a2
Nicholas Tsirakis <tsirakisn@ainfosec.com>
2019-05-31 12:31:03 -0400

    COME ON MAN

--- argo-linux/argo-module.c
+++ argo-linux/argo-module.c
@@ -3111,11 +3111,6 @@ allocate_fd_with_private (void *private)
     ind->i_gid = current_fsgid();
 
     DEBUG_APPLE;
-    // FINDME: this is where the argo file gets created (called
-    // in top-level argo_accept() in kernel space)
-    // This file should have the argo fops attached to it and
-    // gets installed to the corresponding fd below
-    printk(KERN_ERR "FINDME: creating argo file");
     f = alloc_file_pseudo(ind, argo_mnt, name, O_RDWR, &argo_fops_stream);
     if ( !f )
     {
@@ -3124,10 +3119,6 @@ allocate_fd_with_private (void *private)
     }
 
     f->private_data = private;
-    f->f_flags = O_RDWR;
- 
-    printk(KERN_ERR "FINDME2: accept returning fd %d", fd);
-
     fd_install (fd, f);
 
     return fd;
